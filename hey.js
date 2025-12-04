38024ae6f31dd4e329a9c688d677f065f10682ba
audytor15CKSource01 <audytor15+CKSource01@securitum.pl>
2025-12-04 17:32:33 +0100

    Update hey.js

--- hey.js
+++ hey.js
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Simple JS Prompt</title>
-</head>
-<body>
-  <script>
-    // Prompt the user for their name
-    let name = prompt("What's your name?");
-
-    // Show a message
-    alert("Hello, I'm from external site" + (name ? ", " + name : "") + "!");
-  </script>
-</body>
-</html>
+document.body.style.background = "red";
+alert("hey, I'm external");
